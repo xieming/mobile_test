a1ce308a3f8c20d93b282e4d2ab2d931b23781ab
Anderson.Xie <passionboyxie@163.com>
2017-01-16 18:55:45 +0800

    update some codes

--- appiumtest/Public/InstallAPK.py
+++ appiumtest/Public/InstallAPK.py
@@ -7,14 +7,16 @@ import lib.Utils as U
 import threading
 from multiprocessing import Queue
 import os
+import CheckEnvironment
+import yaml
 
 
 class Ia:
     def __init__(self, all_result_path, device):
         """
-        Queue模块是用于进程间通信的模块
-        :param all_result_path: 本次测试创建的文件夹
-        :param device: 设备id
+        Queue progress
+        :param all_result_path: folder for this test
+        :param device: device id
         """
         self.all_result_path = all_result_path
         self.device = device
@@ -24,122 +26,163 @@ class Ia:
     @U.l()
     def __uidump(self):
         """
-        获取当前Activity控件树
-        :return:xml在电脑内的地址存储地址
+        get current Activity tree
+        :return:xml on the path of computer
         """
         save_path = self.all_result_path + "/dump.xml"
         self.adb.get_focused_package_xml(save_path)
         return save_path
+    #
+    # @U.l()
+    # def __element(self):
+    #     """
+    #     element has the same attribute, return a signal point
+    #     button_list:conform button,agree,button id
+    #     """
+    #     button0 = 'com.android.packageinstaller:id/ok_button'
+    #     button1 = 'com.android.packageinstaller:id/btn_allow_once'
+    #     button2 = 'com.android.packageinstaller:id/bottom_button_two'
+    #     button3 = 'com.android.packageinstaller:id/btn_continue_install'
+    #     button4 = 'android:id/button1'
+    #     button5 = 'vivo:id/vivo_adb_install_ok_button'
+    #     button_list = [button0, button1, button2, button3, button4, button5]
+    #     self.__uidump()
+    #     self.pattern = re.compile(r"\d+")
+    #     if not os.path.exists(self.all_result_path + "/dump.xml"):
+    #         U.Logging.warn('Failed to get xml')
+    #         return None
+    #
+    #     tree = ET.ElementTree(file=self.all_result_path + "/dump.xml")
+    #     tree_iter = tree.iter(tag="node")
+    #     for elem in tree_iter:
+    #         if elem.attrib["resource-id"] in button_list:
+    #             bounds = elem.attrib["bounds"]
+    #             coord = self.pattern.findall(bounds)
+    #             x_point = (int(coord[2]) - int(coord[0])) / 2.0 + int(coord[0])
+    #             y_point = (int(coord[3]) - int(coord[1])) / 2.0 + int(coord[1])
+    #             return x_point, y_point
+    #     else:
+    #         return None
 
-    @U.l()
-    def __element(self):
-        """
-        同属性单个元素，返回单个坐标元组
-        button_list:常见的确认,同意,按钮控件id
-        """
-        button0 = 'com.android.packageinstaller:id/ok_button'
-        button1 = 'com.android.packageinstaller:id/btn_allow_once'
-        button2 = 'com.android.packageinstaller:id/bottom_button_two'
-        button3 = 'com.android.packageinstaller:id/btn_continue_install'
-        button4 = 'android:id/button1'
-        button5 = 'vivo:id/vivo_adb_install_ok_button'
-        button_list = [button0, button1, button2, button3, button4, button5]
-        self.__uidump()
-        self.pattern = re.compile(r"\d+")
-        if not os.path.exists(self.all_result_path + "/dump.xml"):
-            U.Logging.warn('Failed to get xml')
-            return None
+    # def tap(self):
+    #
+    #     coordinate_points = self.__element()
+    #     if coordinate_points is not None:
+    #         self.adb.touch_by_element(coordinate_points)
 
-        tree = ET.ElementTree(file=self.all_result_path + "/dump.xml")
-        tree_iter = tree.iter(tag="node")
-        for elem in tree_iter:
-            if elem.attrib["resource-id"] in button_list:
-                bounds = elem.attrib["bounds"]
-                coord = self.pattern.findall(bounds)
-                x_point = (int(coord[2]) - int(coord[0])) / 2.0 + int(coord[0])
-                y_point = (int(coord[3]) - int(coord[1])) / 2.0 + int(coord[1])
-                return x_point, y_point
-        else:
-            return None
+    # def tap_all(self):
+    #     """
+    #     get xml contiunly,add click with mutipli threads
+    #     :return:
+    #     """
+    #     while True:
+    #         self.tap()
+    #         if not self.queue.empty():
+    #             break
+    #
+    # @U.l()
+    # def __install_app(self, package_name, app_file_path):
+    #     """
+    #     :param package_name: package name:com:x.x
+    #     :param app_file_path: the path of the package, absloutely path
+    #     :return:
+    #     """
+    #     self.adb.quit_app(
+    #         'com.android.packageinstaller')
+    #     if self.queue.empty():
+    #         if self.adb.is_install(package_name):
+    #             U.Logging.success(
+    #                 'del {}-{}'.format(self.device, package_name))
+    #             self.adb.remove_app(package_name)
+    #         install_num = 0
+    #         while install_num < 4:
+    #             install_info = self.adb.install_app(app_file_path).stdout.readlines()
+    #             U.Logging.success('install_info:%s'%install_info)
+    #             if self.adb.is_install(package_name):
+    #                 self.queue.put(1)
+    #                 break
+    #             else:
+    #                 U.Logging.error('Reinstalling %s %s '%(package_name,self.device))
+    #                 install_num += 1
+    #         else:
+    #             raise AssertionError('Reinstalling app error')
+    #
+    #         # kill application
+    #         self.adb.quit_app('com.android.packageinstaller')
+    #
+    # def main(self):
+    #     """
+    #     start mutiply threads:
+    #             thread 1: install application
+    #             thread 2: get current page wheather has clickble button
+    #     :return:
+    #     """
+    #     ini = U.ConfigIni()
+    #     install_file = ini.get_ini('test_install_path', 'path')
+    #     package_name = ini.get_ini('test_package_name', 'package_name')
+    #
+    #     threads = []
+    #
+    #     click_button = threading.Thread(target=self.tap_all, args=())
+    #     threads.append(click_button)
+    #     install_app = threading.Thread(
+    #         target=self.__install_app, args=(
+    #             package_name, install_file))
+    #     threads.append(install_app)
+    #     process_list = range(len(threads))
+    #
+    #     for i in process_list:
+    #         threads[i].start()
+    #     for i in process_list:
+    #         threads[i].join()
+    #
+    #     self.adb.shell('"rm -r /data/local/tmp/*.xml"')
+    def get_apk_name(self):
+        dir = os.path.split(__file__)[0].replace('Public', 'Data')
 
-    def tap(self):
-        """
-        点击动作
-        :return:
-        """
-        coordinate_points = self.__element()
-        if coordinate_points is not None:
-            self.adb.touch_by_element(coordinate_points)
-
-    def tap_all(self):
-        """
-        不间断获取xml,并且点击。配合多线程使用
-        :return:
-        """
-        while True:
-            self.tap()
-            if not self.queue.empty():
-                break
-
-    @U.l()
-    def __install_app(self, package_name, app_file_path):
-        """
-        :param package_name: 应用的报名:com:x.x
-        :param app_file_path: 应用的安装路径,注意需要绝对路径
-        :return:
-        """
-        self.adb.quit_app(
-            'com.android.packageinstaller')  # kill安装程序,用于处理oppo的一个bug
-        if self.queue.empty():
-            if self.adb.is_install(package_name):
-                U.Logging.success(
-                    'del {}-{}'.format(self.device, package_name))
-                self.adb.remove_app(package_name)
-            install_num = 0
-            while install_num < 4:
-                install_info = self.adb.install_app(app_file_path).stdout.readlines()
-                U.Logging.success('install_info:%s'%install_info)
-                if self.adb.is_install(package_name):
-                    self.queue.put(1)
-                    break
-                else:
-                    U.Logging.error('Reinstalling %s %s '%(package_name,self.device))
-                    install_num += 1
-            else:
-                raise AssertionError('Reinstalling app error')
-
-            # kill安装程序,用于处理oppo的一个bug
-            self.adb.quit_app('com.android.packageinstaller')
-
-    def main(self):
-        """
-        开启多线程:
-                线程1:安装应用
-                线程2:获取当前页面是否有可点击的按钮
-        :return:
-        """
         ini = U.ConfigIni()
-        install_file = ini.get_ini('test_install_path', 'path')
-        package_name = ini.get_ini('test_package_name', 'package_name')
 
-        threads = []
+        with open(dir + ini.get_ini('apk_info', 'apk_info_path'), 'r') as f:
+            apk_name =yaml.load(f)
+            f.close()
+        return apk_name
 
-        click_button = threading.Thread(target=self.tap_all, args=())
-        threads.append(click_button)
-        install_app = threading.Thread(
-            target=self.__install_app, args=(
-                package_name, install_file))
-        threads.append(install_app)
-        process_list = range(len(threads))
+    def install_apks(self,package_name):
+        U.cmd('adb install {}'.format(self.all_result_path + package_name))
 
-        for i in process_list:
-            threads[i].start()
-        for i in process_list:
-            threads[i].join()
+    def cover_install_apks(self,package_name):
+        U.cmd('adb install -r {}'.format(self.all_result_path+package_name))
 
-        self.adb.shell('"rm -r /data/local/tmp/*.xml"')
+    def find_apks(self):
+        apks = []
+        for c in os.listdir(self.all_result_path):
+            if c.endswith('.apk'):
+                apks.append(c)
+        return apks
+
+    def install(self,package_name):
+        dir = os.path.split(__file__)[0].replace('Public', 'Data')
+
+        ini = U.ConfigIni()
+        apk_path = dir + ini.get_ini('apk_location', 'apk_path')
+
+        if not os.listdir(apk_path):
+            print "please check you apk folder"
+        else:
+            print self.find_apks()
+            if self.find_apks() !=[]:
+                print "find the file"
+                self.install_apks(package_name)
+            else:
+                print "cannot find apk file"
+
 
 
 if __name__ == '__main__':
-    a = Ia('/Users/joko/Desktop/temp', 'VGAMCQEI99999999')
-    a.main()
+    CheckEnvironment.check_environment()
+
+
+    a = Ia('/Users/anderson/work/autotest/mobile_test/mobile_test/appiumtest/Data/apk/', 'A7J5T15A28007700')
+    apk_name =a.get_apk_name()
+    print apk_name
+    a.install(apk_name)
--- appiumtest/Public/jenkins_helper.py
+++ appiumtest/Public/jenkins_helper.py
@@ -1,66 +1,138 @@
-
-
-import jenkins
+#
+#
+# import jenkins
+# import os
+# import urllib2
+#
+#
+# class Constants(object):
+#     ANDROID_BUILD_SERVER = 'http://10.128.42.155:8080'
+#     ANDROID_APK_SHORT_NAME = {'B2B': 'engage-corporate',
+#                               'B2C': 'engage-englishtown'}
+#
+#
+# class BuildMode(object):
+#     DEBUG = 'debug'
+#     RELEASE = 'release'
+#
+#
+# class AndroidBuildJobs(object):
+#     ANDROID_DAILY_BUILD = 'engage-android-snapshot'
+#     ANDROID_RELEASE_BUILD = 'engage-android-release'
+#
+#
+# def get_android_last_successful_build(android_job_name, env, is_B2B, mode=BuildMode.DEBUG):
+#     returned_apk_name = ''
+#     returned_apk_full_path = ''
+#
+#     android_build_server = jenkins.Jenkins(Constants.ANDROID_BUILD_SERVER)
+#     last_successful_build_number = android_build_server.get_job_info(android_job_name)['lastSuccessfulBuild']['number']
+#     build_info = android_build_server.get_job_info(android_job_name, last_successful_build_number)
+#     build_number_root_url = build_info['lastSuccessfulBuild']['url']
+#
+#     if is_B2B:
+#         short_apk_name = Constants.ANDROID_APK_SHORT_NAME['B2B'] + '-' + env + '-' + mode
+#     else:
+#         short_apk_name = Constants.ANDROID_APK_SHORT_NAME['B2C'] + '-' + env + '-' + mode
+#
+#     all_build_facts = build_info['lastSuccessfulBuild']['artifacts']
+#     for fact in all_build_facts:
+#         if short_apk_name in fact['fileName']:
+#             returned_apk_name = fact['fileName']
+#             returned_apk_full_path = build_number_root_url + 'artifact/' + fact['relativePath']
+#             break
+#
+#     if returned_apk_name == '':
+#         assert False, "Failed to find successful build."
+#
+#     return returned_apk_name, returned_apk_full_path
+#
+#
+# def download_apk(apk_build_name, apk_full_path):
+#     home_dir = os.getcwd()
+#     path = home_dir + '\\' + 'apk_files'
+#     if not os.path.exists(path):
+#         os.makedirs(path)
+#
+#     path = path + '\\' + apk_build_name
+#     f = urllib2.urlopen(apk_full_path)
+#     data = f.read()
+#     with open(path, 'wb') as code:
+#         code.write(data)
+#
+#     if not os.path.exists(path):
+#         assert False, "Failed to download to local path."
+#
+#     return path
 import os
-import urllib2
+import re
+import shutil
+import subprocess
+import time
+import yaml
+
+import lib.Utils as U
 
 
-class Constants(object):
-    ANDROID_BUILD_SERVER = 'http://10.128.42.155:8080'
-    ANDROID_APK_SHORT_NAME = {'B2B': 'engage-corporate',
-                              'B2C': 'engage-englishtown'}
 
+current_dir = os.path.split(os.path.realpath(__file__))[0]
 
-class BuildMode(object):
-    DEBUG = 'debug'
-    RELEASE = 'release'
-
-
-class AndroidBuildJobs(object):
-    ANDROID_DAILY_BUILD = 'engage-android-snapshot'
-    ANDROID_RELEASE_BUILD = 'engage-android-release'
-
-
-def get_android_last_successful_build(android_job_name, env, is_B2B, mode=BuildMode.DEBUG):
-    returned_apk_name = ''
-    returned_apk_full_path = ''
-
-    android_build_server = jenkins.Jenkins(Constants.ANDROID_BUILD_SERVER)
-    last_successful_build_number = android_build_server.get_job_info(android_job_name)['lastSuccessfulBuild']['number']
-    build_info = android_build_server.get_job_info(android_job_name, last_successful_build_number)
-    build_number_root_url = build_info['lastSuccessfulBuild']['url']
-
-    if is_B2B:
-        short_apk_name = Constants.ANDROID_APK_SHORT_NAME['B2B'] + '-' + env + '-' + mode
+def exec_command(cmd):
+    result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    (stdoutdata, stderrdata) = result.communicate()
+    if (re.search("error", str(stdoutdata))):
+        print "error occur!"
     else:
-        short_apk_name = Constants.ANDROID_APK_SHORT_NAME['B2C'] + '-' + env + '-' + mode
+        return stdoutdata
 
-    all_build_facts = build_info['lastSuccessfulBuild']['artifacts']
-    for fact in all_build_facts:
-        if short_apk_name in fact['fileName']:
-            returned_apk_name = fact['fileName']
-            returned_apk_full_path = build_number_root_url + 'artifact/' + fact['relativePath']
-            break
-
-    if returned_apk_name == '':
-        assert False, "Failed to find successful build."
-
-    return returned_apk_name, returned_apk_full_path
+def get_url(url, folder_name):
+    os.chdir(folder_name)
+    if not os.path.exists(folder_name):
+        print "please check you folder"
+    if os.listdir(folder_name):
+        for file in os.listdir(folder_name):
+            if file.endswith('.apk'):
+                os.remove(file)
 
 
-def download_apk(apk_build_name, apk_full_path):
-    home_dir = os.getcwd()
-    path = home_dir + '\\' + 'apk_files'
-    if not os.path.exists(path):
-        os.makedirs(path)
+    cmd = 'curl -O {}'.format(url)
+    exec_command(cmd)
+    os.chdir(os.path.split(__file__)[0])
 
-    path = path + '\\' + apk_build_name
-    f = urllib2.urlopen(apk_full_path)
-    data = f.read()
-    with open(path, 'wb') as code:
-        code.write(data)
 
-    if not os.path.exists(path):
-        assert False, "Failed to download to local path."
+# dir = os.path.split(__file__)[0].replace('Public', 'Data')
+#
+# ini = U.ConfigIni()
+# apk_path = dir + ini.get_ini('test_device', 'device')
+# print apk_path
 
-    return path
+# def install_apks(apk_name):
+#     cmd = 'adb install {0}'.format(apk_name)
+#     print cmd
+#     result = exec_command(cmd)
+#     print result
+#     if (re.search("success", result)):
+#         print "install success!"
+#     else:
+#         print "install fail!"
+
+def download():
+    dir = os.path.split(__file__)[0].replace('Public', 'Data')
+
+    ini = U.ConfigIni()
+    apk_path = dir + ini.get_ini('apk_location', 'apk_path')
+    print apk_path
+
+    apk_url = ini.get_ini('apk_url', 'apk_jenkins_url')
+    apk_name = apk_url.split("/")[-1]
+    with open(dir + ini.get_ini('apk_info', 'apk_info_path'), 'w') as f:
+        yaml.dump(apk_name, f)
+        f.close()
+    get_url(apk_url,apk_path)
+
+def main():
+
+    download()
+
+if __name__ == '__main__':
+    main()
