8c016b82264d6bbec74d2d5124ed4b33191a6f7a
ming.xiesh <ming.xiesh@ef.com>
2017-02-15 12:12:17 +0800

    update selenium

--- confluence.py
+++ confluence.py
@@ -21,7 +21,7 @@ import time
 import sys
 
 Emails = []
-ID = []
+
 
 def check_time(start_day):
     start_time = datetime.datetime.strptime(start_day.decode(), '%Y-%m-%d')
@@ -82,7 +82,7 @@ def makeTableContentList(table):
         result.append(line)
     return result
 
-def get_all_email():
+def get_all_email(ID,type):
     login_page = "https://confluence.englishtown.com/"
     devive_page = "https://confluence.englishtown.com/pages/viewpage.action?pageId=673644924"
     driver = webdriver.PhantomJS()
@@ -93,16 +93,18 @@ def get_all_email():
     driver.find_element_by_id('loginButton').click()
     driver.get(devive_page)
     print (driver.title)
+
+    if type == "Android":
+        path = "//div[2]/table/tbody/tr[{}]/td[8]/div//a"
+    elif type == "IOS":
+        path = "//div[3]/table/tbody/tr[{}]/td[6]/div//a"
     if ID:
         print(ID)
         for id in ID:
             print('each')
             print(id)
 
-            ele = driver.find_element_by_xpath("//div[2]/table/tbody/tr[{}]/td[8]/div//a".format(id))
-
-
-
+            ele = driver.find_element_by_xpath(path.format(id))
 
             time.sleep(1)
             print (ele.text.replace(" ",".")+"@ef.com")
@@ -148,25 +150,38 @@ def sort_by_count(d):
     d=collections.OrderedDict(sorted(list(d.items()),key = lambda t: -t[1]))
     return d
 
+def check_tables(result):
+
+    index = 1
+    ID = []
+    for kk in result[1:]:
+        print(kk[-3].decode())
+
+        if kk[-3].decode() != "\xa0" and kk[-2].decode() != "\xa0":
+            #
+            # print (kk[-2].decode())
+            # print (check_time(kk[-2]))
+
+            if check_time(kk[-2]) > 60:
+                # print ("why")
+                ID.append(index)
+                print("ok %s" % (kk[0]))
+                # print("nok %s" % (kk[-2]))
+                # print("nok %s" % (kk[-3]))
+        else:
+            pass
+
+        index = index + 1
+
+    return ID
+        # print result
+
+
+
 
 def main():
 
-
-
-    # loadConfluencePage(pageID)
-    # print htmlContent
-
-
-    #make output directory
-    #mkdir(sys.path[0] + "/output")
-
-    # there are two pages contain data model
     pageID = "673644924"
-
-
-
-    print("Make data in page with id: ", pageID)
-
     htmlContent = loadConfluencePage(pageID)
 
     soup = BeautifulSoup(htmlContent)
@@ -174,34 +189,22 @@ def main():
 
     tables = soup.findAll('table')
 
-    tables.pop()
-    print(len(tables))
 
-    result_table = []
-    for table in tables:
-        #print table
-        result = makeTableContentList(table)
-        index = 1
-        for kk in result[1:]:
-            print (kk[-3].decode())
 
-            if kk[-3].decode() !="\xa0" and kk[-2].decode() !="\xa0":
-                #
-                # print (kk[-2].decode())
-                # print (check_time(kk[-2]))
+    types = ["Android","IOS"]
 
-                if check_time(kk[-2]) > 60:
-                    #print ("why")
-                    ID.append(index)
-                    print("ok %s" %(kk[0]))
-                    # print("nok %s" % (kk[-2]))
-                    # print("nok %s" % (kk[-3]))
-            else:
-                pass
+    for type in types:
+        if type == "Android":
+            Android_result = makeTableContentList(tables[0])
+            print(Android_result)
+            ID=check_tables(Android_result)
+        elif type =="IOS":
+            IOS_result = makeTableContentList(tables[1])
+            ID=check_tables(IOS_result)
+        print (ID)
+        get_all_email(ID, type)
 
-            index = index +1
-        #print result
-        result_table.append(result)
+
     #     makeFile(result)
     #         # print "result = "
     #         # print result
@@ -211,30 +214,31 @@ def main():
     android_mobile_number = 0
     android_tablet_number = 0
 
-    print("androd number is: {}".format(len(result_table[0]) -1 ))
+    print("android number is: {}".format(len(Android_result) -1 ))
 
     type = []
     i =0
-    for detail in result_table[0]:
+    for detail in Android_result:
 
         if i is not 0:
+            print (detail)
 
             type.append(detail[5])
-            # if table[7].fainAll('a class'):
-            #     name = table[7].text
-            #
-            #     print name
-            #
-            #
-            #
-            # else:
-            #     name = table[7].text
-            #
-            #     print name
-
-        if 'Tablet' in detail:
+    #         # if table[7].fainAll('a class'):
+    #         #     name = table[7].text
+    #         #
+    #         #     print name
+    #         #
+    #         #
+    #         #
+    #         # else:
+    #         #     name = table[7].text
+    #         #
+    #         #     print name
+    #
+        if b'Tablet' in detail:
             android_tablet_number +=1
-        if  'Phone' in detail:
+        if  b'Phone' in detail:
             android_mobile_number +=1
 
         #
@@ -242,19 +246,18 @@ def main():
 
         i +=1
 
-    # print("tablet number is : {}".format(android_tablet_number))
-    # print("mobile number is : {}".format(android_mobile_number))
+    print("tablet number is : {}".format(android_tablet_number))
+    print("mobile number is : {}".format(android_mobile_number))
     #
-    # ios_mobile_number = len(result_table[0]) - 1
-    # print("ios phone is: {}".format(ios_mobile_number))
-    #
-    # print(type)
-    # ll =count(type)
-    #
-    # print(ll)
-    # gg = sort_by_count(ll)
-    # print(gg)
-    get_all_email()
+    ios_mobile_number = len(IOS_result) - 1
+    print("ios phone is: {}".format(ios_mobile_number))
+
+    print(type)
+    ll =count(type)
+
+    print(ll)
+    gg = sort_by_count(ll)
+    print(gg)
 
 if __name__ == "__main__":
     main()
